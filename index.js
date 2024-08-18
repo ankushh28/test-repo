bf5d579018ed49374a2d5e548ce49820083ea2d8
Ankush Choudhary <74401816+ankushh28@users.noreply.github.com>
2024-08-18 15:34:30 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
 
-// Middleware to parse JSON bodies
 app.use(express.json());
 
-// POST route to return the JSON request body
 app.post('/success', (req, res) => {
     res.json(req.body);
 });
