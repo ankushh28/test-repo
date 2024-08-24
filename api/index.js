9965d2b5b53dbaf974de6aabe3264ee862e63661
Ankush Choudhary <74401816+ankushh28@users.noreply.github.com>
2024-08-24 16:47:51 +0530

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const app = express();
 
-// Middleware to parse JSON bodies
+
 app.use(express.json());
 
-// POST route to return the JSON request body
+
+
+
 app.post('/success', (req, res) => {
 const payload = req.body;
   console.log('Received PayU webhook:', payload);
   res.status(200).send(payload);
 });
 
-// Start the server
+
+
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
